hafenkran/vcpkg

commit 3f6674e4541334ea4d7f92120788d1e10f9d5ed1

[curl] Update to 7.66.0 (#7331)
* [curl] Update to 7.65.3

* [curl] Remove USAGE

(Use auto-generated message about exported config.)

* Add wrapper

* [curl] Update to 7.66.0

* Add wrapper to curl

* drop parent_scope

* add change that didn't make it

* Populate CURL_LIBRARY

* Fix transitive dependency

* try to actually set LIBRARIES

* get .libs

* fix curlpp and dynamic builds on linux

* add @cenit's suggestion and fix typo in curlpp

* modify PDAL patch

ports/curlpp/vcpkg-cmake-wrapper.cmake
@@ -1,35 +1,0 @@
-_find_package(${ARGS})
-
-if("@VCPKG_LIBRARY_LINKAGE@" STREQUAL "static")
-    if(TARGET unofficial::curlpp::curlpp)
-        # Fix CURL dependencies. See:
-        # https://github.com/Microsoft/vcpkg/issues/4312
-
-        set(_libs "")
-
-        find_package(CURL REQUIRED)
-
-        set(ZLIB_ROOT ${CMAKE_PREFIX_PATH}) # Prefer Zlib installed via `vcpkg`
-        find_package(ZLIB)
-        unset(ZLIB_ROOT)
-
-        list(APPEND _libs ${CURL_LIBRARIES} ZLIB::ZLIB)
-
-        find_package(OpenSSL QUIET)
-        if(OPENSSL_FOUND)
-            list(APPEND _libs OpenSSL::SSL OpenSSL::Crypto)
-        endif()
-
-        find_package(Threads REQUIRED)
-        list(APPEND _libs Threads::Threads)
-
-        if(CMAKE_SYSTEM_NAME STREQUAL "Windows" OR CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-            list(APPEND _libs Ws2_32 Crypt32)
-        endif()
-
-        set_target_properties(
-            unofficial::curlpp::curlpp
-            PROPERTIES INTERFACE_LINK_LIBRARIES "${_libs}"
-        )
-    endif()
-endif()

ports/curl/CONTROL
@@ -1,5 +1,5 @@
 Source: curl
-Version: 7.65.2-1
+Version: 7.66.0
 Build-Depends: zlib
 Homepage: https://github.com/curl/curl
 Description: A library for transferring data with URLs

ports/curl/vcpkg-cmake-wrapper.cmake
@@ -1,0 +1,42 @@
+list(REMOVE_ITEM ARGS "NO_MODULE")
+list(REMOVE_ITEM ARGS "CONFIG")
+list(REMOVE_ITEM ARGS "MODULE")
+
+_find_package(${ARGS} CONFIG)
+
+if(TARGET CURL::libcurl)
+    set(CURL_FOUND TRUE)
+
+    get_target_property(_curl_include_dirs CURL::libcurl INTERFACE_INCLUDE_DIRECTORIES)
+    get_target_property(_curl_link_libraries CURL::libcurl INTERFACE_LINK_LIBRARIES)
+
+    if (CMAKE_SYSTEM_NAME STREQUAL "Windows" OR CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+        get_target_property(_curl_location_debug CURL::libcurl IMPORTED_IMPLIB_DEBUG)
+        get_target_property(_curl_location_release CURL::libcurl IMPORTED_IMPLIB_RELEASE)
+    endif()
+
+    if(NOT _curl_location_debug AND NOT _curl_location_release)
+        get_target_property(_curl_location_debug CURL::libcurl IMPORTED_LOCATION_DEBUG)
+        get_target_property(_curl_location_release CURL::libcurl IMPORTED_LOCATION_RELEASE)
+    endif()
+
+    if(NOT _curl_link_libraries)
+        set(_curl_link_libraries)
+    endif()
+
+    set(CURL_INCLUDE_DIRS "${_curl_include_dirs}")
+    set(CURL_LIBRARY_DEBUG "${_curl_location_debug}")
+    set(CURL_LIBRARY_RELEASE "${_curl_location_release}")
+
+    #For builds which rely on CURL_LIBRAR(Y/IES)
+    include(${CMAKE_ROOT}/Modules/SelectLibraryConfigurations.cmake)
+    select_library_configurations(CURL)
+
+    set(CURL_LIBRARIES ${CURL_LIBRARY} ${_curl_link_libraries})
+    set(CURL_VERSION_STRING "${CURL_VERSION}")
+
+    set(_curl_include_dirs)
+    set(_curl_link_libraries)
+    set(_curl_location_debug)
+    set(_curl_location_release)
+endif()

ports/curl/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO curl/curl
-    REF curl-7_65_2
-    SHA512 8e06377a6d8837a4c2cd96f978f0ac848b9472500fd25983bb1f9e5f52d9d6f7ff0c71d443587a979cf80fd19412bb64b9362b774cf91e02479fdfad7e085b16
+    REF curl-7_66_0
+    SHA512 0b02b6117ab69e848219fa59b25e4254934d841fbc3e2a70ccfdcb417ab7fc46a10dd703a25fbe8a2a98a59cbc73ad6b9298f5668a7345f7910010b958de4d21
     HEAD_REF master
     PATCHES
         0001_cmake.patch
@@ -175,6 +175,16 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 else()
     file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/curl-config ${CURRENT_PACKAGES_DIR}/debug/bin/curl-config)
+
+    file(GLOB FILES LIST_DIRECTORIES TRUE ${CURRENT_PACKAGES_DIR}/bin/*)
+    if (FILES STREQUAL "")
+        file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+    endif()
+
+    file(GLOB FILES LIST_DIRECTORIES TRUE ${CURRENT_PACKAGES_DIR}/debug/bin/*)
+    if (FILES STREQUAL "")
+        file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
+    endif()
 endif()
 
 file(READ ${CURRENT_PACKAGES_DIR}/include/curl/curl.h CURL_H)
@@ -185,8 +195,8 @@ else()
 endif()
 file(WRITE ${CURRENT_PACKAGES_DIR}/include/curl/curl.h "${CURL_H}")
 
+file(INSTALL ${CURRENT_PORT_DIR}/vcpkg-cmake-wrapper.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/curl)
+
 vcpkg_copy_pdbs()
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-
-vcpkg_test_cmake(PACKAGE_NAME CURL MODULE)
+vcpkg_test_cmake(PACKAGE_NAME CURL)

ports/curl/usage
@@ -1,5 +1,0 @@
-The package curl is compatible with built-in CMake targets:
-
-    find_package(CURL REQUIRED)
-    target_link_libraries(main PRIVATE ${CURL_LIBRARIES})
-    target_include_directories(main PRIVATE ${CURL_INCLUDE_DIRS})

ports/curlpp/portfile.cmake
@@ -29,12 +29,6 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
         ${CURRENT_PACKAGES_DIR}/bin
         ${CURRENT_PACKAGES_DIR}/debug/bin
     )
-
-    configure_file(
-        ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake
-        ${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}
-        @ONLY
-    )
 endif()
 
 # Handle copyright

ports/pdal/PDALConfig.patch
@@ -2,12 +2,13 @@ diff --git a/PDALConfig.cmake.in b/PDALConfig.cmake.in
 index a03ef142e..2d06a2937 100644
 --- a/PDALConfig.cmake.in
 +++ b/PDALConfig.cmake.in	
-@@ -15,6 +15,8 @@ foreach(_dir @PDAL_CONFIG_LIBRARY_DIRS@)
+@@ -15,6 +15,9 @@ foreach(_dir @PDAL_CONFIG_LIBRARY_DIRS@)
    list(APPEND PDAL_LIBRARY_DIRS ${_foo})
  endforeach(_dir)
  
 +include(CMakeFindDependencyMacro)
 +find_dependency(GeoTIFF)
++find_dependency(CURL)
  include("${CMAKE_CURRENT_LIST_DIR}/PDALTargets.cmake")
  
  if (WIN32)

commit 582093442a28bc0518ad7742fb3caaa02a0c651c

[curlpp] fix regression introduced in #7331, commit 7358190c3be474dd3c9640a98050bdc653c841b3

ports/curlpp/CONTROL
@@ -1,4 +1,4 @@
 Source: curlpp
-Version: 2018-06-15
+Version: 2018-06-15-1
 Description: C++ wrapper around libcURL
 Build-Depends: curl

ports/curlpp/fix-cmake.patch
@@ -29,7 +29,7 @@ index 8b183a0..a801ae8 100644
 -SET_TARGET_PROPERTIES(${PROJECT_NAME}_static PROPERTIES PREFIX "lib")
 -target_link_libraries(${PROJECT_NAME}_static ${CURL_LIBRARIES} ${CONAN_LIBS})
 +SET_TARGET_PROPERTIES(${PROJECT_NAME} PROPERTIES PREFIX "lib")
-+target_link_libraries(${PROJECT_NAME} ${CURL_LIBRARIES} ${CONAN_LIBS})
++target_link_libraries(${PROJECT_NAME} CURL::libcurl)
 +endif()
 +
 +target_include_directories(${PROJECT_NAME} PUBLIC $<INSTALL_INTERFACE:include>)

ports/curlpp/vcpkg-cmake-wrapper.cmake
@@ -1,0 +1,5 @@
+set(FIND_CURLPP_ARGS ${ARGS})
+include(CMakeFindDependencyMacro)
+find_dependency(CURL)
+
+_find_package(${FIND_CURLPP_ARGS})
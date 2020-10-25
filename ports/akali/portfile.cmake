hafenkran/vcpkg

commit a97e32fa2de760252ec401f21d82d0cd3dd402c1

[netcdf-c/nvtt] Update to the latest version (#13674)

ports/nvtt/CONTROL
@@ -1,3 +1,5 @@
 Source: nvtt
-Version: 2.1.1
+Version: 2.1.2
+Homepage: https://github.com/castano/nvidia-texture-tools
 Description: Texture processing tools with support for Direct3D 10 and 11 formats.
+Supports: !x86

ports/nvtt/portfile.cmake
@@ -1,12 +1,12 @@
-include(vcpkg_common_functions)
+vcpkg_fail_port_install(ON_ARCH "x86")
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO castano/nvidia-texture-tools
-    REF 2.1.1
-    SHA512 3e6fef5977ca29daa7dc97afe11d61de57a8556c9caf30902db8c5c167d9c38f736bcb62eebdaaf7558299b39975bc269d41ab980c813b67dd1fc85064c853c9
+    REF b1a90f36013522b9e5a3a2197859b72188752e3f  # 2.1.2
+    SHA512 13cf04a9856f150f7569c8c256c42fc6a5bc40586c9ca2b3ae553edf5bfcbccbba5b8538924079ed35effdd07b9e3ef4bfdb9733a2ec51f5a95f958885cc6cca
     HEAD_REF master
     PATCHES
         001-define-value-for-HAVE_UNISTD_H-in-mac-os.patch
@@ -14,6 +14,7 @@ vcpkg_from_github(
         bc7.patch
         squish.patch
         fix-build-error.patch
+        add-compile-options-for-osx.patch
 )
 
 vcpkg_configure_cmake(
@@ -37,5 +38,4 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 # Handle copyright
 file(REMOVE ${CURRENT_PACKAGES_DIR}/share/doc/nvtt/LICENSE)
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/nvtt)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/nvtt/LICENSE ${CURRENT_PACKAGES_DIR}/share/nvtt/copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)

ports/netcdf-c/CONTROL
@@ -1,6 +1,5 @@
 Source: netcdf-c
-Version: 4.7.3
-Port-Version: 4
+Version: 4.7.4
 Build-Depends: hdf5, curl
 Homepage: https://github.com/Unidata/netcdf-c
 Description: a set of self-describing, machine-independent data formats that support the creation, access, and sharing of array-oriented scientific data.

ports/netcdf-c/portfile.cmake
@@ -1,14 +1,13 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Unidata/netcdf-c
-    REF b7cd387bee8c661141fabb490f4969587c008c55 # v4.7.3
-    SHA512 a55391620fac61e4975fe62907ca21049911afce6190fc12d183d24133a32aae8cd223b97a3fe57fc82d8bdca1a7db451046e3be3c379051624d48b1f56c0332
+    REF 26fba54a58fa02af92d84441ed90b417c1d08161 # v4.7.4
+    SHA512 7144374b5bd3574ea422de07ffb30fecc4e5f560f9b46f62762cc0cce511dd33068b8df9244fe94ae3cc7b3a9bb9fe398c7e67c3e5ac2109768e5a9b984f24fb
     HEAD_REF master
     PATCHES
         no-install-deps.patch
         config-pkg-location.patch
         use_targets.patch
-        mpi.patch
         fix-dependency-libmath.patch
 )
 

ports/netcdf-c/config-pkg-location.patch
@@ -1,8 +1,8 @@
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 46892ee..803ff46 100644
+index bd7c4f8..e4e92c6 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -1742,7 +1742,9 @@ FILE(COPY ${netCDF_SOURCE_DIR}/include/netcdf_meta.h
+@@ -2050,7 +2050,9 @@ configure_file(${CMAKE_CURRENT_SOURCE_DIR}/ncdap_test/pingurl.c ${CMAKE_CURRENT_
  # Create CMake package configuration files. With these, other packages using
  # cmake should be able to find netcdf using find_package and find_library.
  # The EXPORT call is paired with one in liblib.

ports/netcdf-c/fix-dependency-libmath.patch
@@ -1,8 +1,8 @@
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 35c694f..e2c03e5 100644
+index bd7c4f8..03b7ccf 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -839,7 +839,7 @@ ENDIF()
+@@ -866,7 +866,7 @@ ENDIF()
  
  # Check for the math library so it can be explicitly linked.
  IF(NOT WIN32)

ports/netcdf-c/mpi.patch
@@ -1,14 +1,0 @@
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 101f97719..b070121b8 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -1004,6 +1004,8 @@ IF(ENABLE_PARALLEL4 AND ENABLE_NETCDF_4)
-     IF(MSVC)
-       FIND_PACKAGE(MPI REQUIRED)
-       INCLUDE_DIRECTORIES(${MPI_C_INCLUDE_PATH})
-+      list(APPEND CMAKE_REQUIRED_LIBRARIES MPI::MPI_C)
-+      list(APPEND EXTRA_DEPS MPI::MPI_C)
-     ENDIF()
-     SET(HDF5_PARALLEL ON CACHE BOOL "")
-     SET(USE_PARALLEL ON CACHE BOOL "")
-

ports/netcdf-c/no-install-deps.patch
@@ -1,8 +1,8 @@
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index e87f3de..46892ee 100644
+index bd7c4f8..95fb01c 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -1570,7 +1570,7 @@ ADD_SUBDIRECTORY(docs)
+@@ -1830,7 +1830,7 @@ ADD_SUBDIRECTORY(docs)
  # install them in the binary dir. Grab all of the .libs, put them
  # in the libdir.
  ##

commit 5e11a4b8cce21c958e0517f7281199daf2c3585b

[Many Ports] Baseline Fixes for 2020-10-24 CI Build (#14208)
* [libp7client, libp7-baical] Combine identical ports and update to libp7client 5.6.

In a recent CI build, we started getting output like:

Installing package libp7-baical[core]:x86-windows...
The following files are already installed in C:/Dev/vcpkg/installed/x86-windows and are in conflict with libp7-baical:x86-windows

Installed by libp7client:x86-windows
    bin/P7x32.dll
    bin/P7x32.pdb
    debug/bin/P7x32d.dll
    debug/bin/P7x32d.pdb
    include/P7/GTypes.h
    include/P7/P7_Client.h
    include/P7/P7_Cproxy.h
    include/P7/P7_Extensions.h
    include/P7/P7_Telemetry.h
    include/P7/P7_Trace.h

It turns out that these ports are identical, they are just different versions of the same library. The libp7-baical version supported more platforms and was simpler, so took that one and updated it to a current version.

* [ignition-modularscripts, ignition-msgs1] Attempt to fix intermittent build failures by disabling parallel configure.

* Fix several "passing: remove from fail list."

* [akali] Disable parallel configure.

REGRESSION: akali:x64-windows. If expected, add akali:x64-windows=fail to .\scripts\ci.baseline.txt.

Attempts to fix:

CMake Error: Could not open file for write in copy operation C:/Dev/vcpkg/buildtrees/akali/src/fc94eb83a9-01654d3f7c.clean/include/akali_config.h.tmp
CMake Error: : System Error: Permission denied
CMake Error at CMakeLists.txt:20 (configure_file):
  configure_file Problem configuring file

* [nvtt] Devendor libsquish

This was regressed by https://github.com/microsoft/vcpkg/pull/13674

REGRESSION: nvtt:x64-linux. If expected, add nvtt:x64-linux=fail to .\scripts\ci.baseline.txt.
REGRESSION: nvtt:x64-osx. If expected, add nvtt:x64-osx=fail to .\scripts\ci.baseline.txt.
REGRESSION: nvtt:x64-windows-static. If expected, add nvtt:x64-windows-static=fail to .\scripts\ci.baseline.txt.

ports/akali/CONTROL
@@ -1,5 +1,6 @@
 Source: akali
 Version: 1.42
+Port-Version: 1
 Description: C++ Common Library.
 Homepage: https://github.com/winsoft666/akali
 Supports: !(arm|arm64|uwp)

ports/akali/portfile.cmake
@@ -13,6 +13,7 @@ string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" AKALI_STATIC)
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         -DAKALI_STATIC:BOOL=${AKALI_STATIC}
         -DBUILD_TESTS:BOOL=OFF

ports/ignition-modularscripts/CONTROL
@@ -1,4 +1,4 @@
 Source: ignition-modularscripts
 Version: 2020-05-16
-Port-Version: 1
+Port-Version: 2
 Description: Vcpkg helpers to package ignition libraries

ports/ignition-modularscripts/ignition_modular_library.cmake
@@ -3,6 +3,7 @@ function(ignition_modular_build_library NAME MAJOR_VERSION SOURCE_PATH CMAKE_PAC
     vcpkg_configure_cmake(
         SOURCE_PATH ${SOURCE_PATH}
         PREFER_NINJA
+        DISABLE_PARALLEL_CONFIGURE
         OPTIONS -DBUILD_TESTING=OFF
     )
 
@@ -29,13 +30,13 @@ function(ignition_modular_build_library NAME MAJOR_VERSION SOURCE_PATH CMAKE_PAC
         file(COPY ${CMAKE_RELEASE_FILES} DESTINATION
                   "${CURRENT_PACKAGES_DIR}/share/${CMAKE_PACKAGE_NAME}/")
     endif()
-    
+
     # Remove unused files files
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/cmake
                         ${CURRENT_PACKAGES_DIR}/debug/include
                         ${CURRENT_PACKAGES_DIR}/debug/lib/cmake
                         ${CURRENT_PACKAGES_DIR}/debug/share)
-    
+
     # Make pkg-config files relocatable
     if(NOT IML_DISABLE_PKGCONFIG_INSTALL)
         if(VCPKG_TARGET_IS_LINUX)
@@ -97,7 +98,7 @@ endfunction()
 ## ### CMAKE_PACKAGE_NAME
 ## The name of the CMake package for the port.
 ## If not specified, defaults to `ignition-${NAME}${MAJOR_VERSION}`.
-## 
+##
 ## ### DISABLE_PKGCONFIG_INSTALL
 ## If present, disable installation of .pc pkg-config configuration files.
 ##
@@ -112,16 +113,16 @@ function(ignition_modular_library)
     set(oneValueArgs NAME VERSION SHA512 REF HEAD_REF CMAKE_PACKAGE_NAME)
 	set(multiValueArgs PATCHES)
     cmake_parse_arguments(IML "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-    
+
     string(REPLACE "." ";" IML_VERSION_LIST ${IML_VERSION})
     list(GET IML_VERSION_LIST 0 IML_MAJOR_VERSION)
 
-    # If the REF option is omitted, use the canonical one 
+    # If the REF option is omitted, use the canonical one
     if(NOT DEFINED IML_REF)
         set(IML_REF "ignition-${IML_NAME}${IML_MAJOR_VERSION}_${IML_VERSION}")
     endif()
-    
-    # If the HEAD_REF option is omitted, use the canonical one 
+
+    # If the HEAD_REF option is omitted, use the canonical one
     if(NOT DEFINED IML_HEAD_REF)
         set(IML_HEAD_REF "ign-${IML_NAME}${IML_MAJOR_VERSION}")
     endif()
@@ -141,7 +142,7 @@ function(ignition_modular_library)
         HEAD_REF ${IML_HEAD_REF}
         PATCHES ${IML_PATCHES}
     )
-    
+
     # Build library
     ignition_modular_build_library(${IML_NAME} ${IML_MAJOR_VERSION} ${SOURCE_PATH} ${IML_CMAKE_PACKAGE_NAME} ${DEFAULT_CMAKE_PACKAGE_NAME} ${IML_DISABLE_PKGCONFIG_INSTALL})
 endfunction()

ports/ignition-msgs1/CONTROL
@@ -1,4 +1,5 @@
 Source: ignition-msgs1
-Version: 1.0.0-1
+Version: 1.0.0
+Port-Version: 2
 Build-Depends: ignition-cmake0, ignition-math4, protobuf
 Description: Middleware protobuf messages for robotics

ports/ignition-msgs1/portfile.cmake
@@ -1,5 +1,3 @@
-include(vcpkg_common_functions)
-
 include(${CURRENT_INSTALLED_DIR}/share/ignitionmodularscripts/ignition_modular_library.cmake)
 
 # Explicitly disable cross-compilation until the upstream discussion

ports/knet/portfile.cmake
@@ -13,6 +13,7 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         -DKNET_BUILD_EXAMPLE:BOOL=OFF
         -DKNET_BUILD_TEST:BOOL=OFF

ports/libnice/CONTROL
@@ -1,5 +1,6 @@
 Source: libnice
-Version: 0.1.15-4
+Version: 0.1.15
+Port-Version: 5
 Homepage: https://nice.freedesktop.org
 Description: Libnice is an implementation of the IETF's Interactive Connectivity Establishment (ICE) standard (RFC 5245) and the Session Traversal Utilities for NAT (STUN) standard (RFC 5389).
 Build-Depends: glib, openssl

ports/libnice/portfile.cmake
@@ -14,6 +14,7 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    DISABLE_PARALLEL_CONFIGURE
     OPTIONS_RELEASE -DOPTIMIZE=1
     OPTIONS_DEBUG -DDEBUGGABLE=1
 )

ports/libp7-baical/CONTROL
@@ -1,6 +1,4 @@
 Source: libp7-baical
-Version: 4.4
-Port-Version: 5
-Homepage: https://baical.net/
-Description: P7 is a library for high-speed sending telemetry & trace data from application
-Supports: !(arm|uwp|osx)
+Version: replaced
+Build-Depends: libp7client
+Description: An old name for the port libp7client; please install libp7client instead.

ports/libp7-baical/portfile.cmake
@@ -1,30 +1,2 @@
-vcpkg_fail_port_install(ON_ARCH "arm" ON_TARGET "uwp" "osx")
-
-set(LIBP7_VERSION 4.4)
-set(LIBP7_HASH 500fefdf6fb01999ddb2accc8309cf8749fb7a65abb98faaf6d71a5ae3da4eac8c00c083905b01f7f6cc973387b605f2c4db0bb007562f76b7ad43b6abe2b91f)
-
-vcpkg_download_distfile(ARCHIVE
-    URLS "http://baical.net/files/libP7_v${LIBP7_VERSION}.zip"
-    FILENAME "libP7_v${LIBP7_VERSION}.zip"
-    SHA512 ${LIBP7_HASH}
-)
-
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-    NO_REMOVE_ONE_LEVEL
-)
-
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-)
-
-vcpkg_install_cmake()
-vcpkg_copy_pdbs()
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
-file(INSTALL ${SOURCE_PATH}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+message(WARNING "libp7-baical has been replaced by libp7client.")

ports/libp7client/CMakeLists.txt
@@ -1,24 +1,17 @@
 project(P7)
-cmake_minimum_required(VERSION 3.0)
+cmake_minimum_required(VERSION 3.18)
 
-set(SOURCES "Sources/ClBaical.cpp"
-            "Sources/ClFile.cpp"
-            "Sources/Client.cpp"
-            "Sources/ClNull.cpp"
-            "Sources/ClText.cpp"
-            "Sources/CRC32.cpp"
-            "Sources/Proxy.cpp"
-            "Sources/Telemetry.cpp"
-            "Sources/Trace.cpp"
-)
+add_library(P7
+  "Sources/ClBaical.cpp"
+  "Sources/ClFile.cpp"
+  "Sources/Client.cpp"
+  "Sources/ClNull.cpp"
+  "Sources/ClText.cpp"
+  "Sources/CRC32.cpp"
+  "Sources/Proxy.cpp"
+  "Sources/Telemetry.cpp"
+  "Sources/Trace.cpp")
 
-if(WIN32)
-  set(RESOURCES "Sources/P7Client.rc")
-else()
-  set(RESOURCES "")
-endif()
-
-add_library(P7 "${SOURCES}" "${RESOURCES}")
 target_include_directories(P7 PRIVATE "Headers/")
 target_include_directories(P7 PRIVATE "Shared/")
 if(WIN32)
@@ -30,7 +23,7 @@ endif()
 
 if(WIN32)
   set_target_properties(P7 PROPERTIES PREFIX "")
-  if(CMAKE_SIZEOF_VOID_P EQUAL 8)   
+  if(CMAKE_SIZEOF_VOID_P EQUAL 8)
     set_target_properties(P7 PROPERTIES DEBUG_POSTFIX "x64d")
     set_target_properties(P7 PROPERTIES RELEASE_POSTFIX "x64")
   else()
@@ -45,9 +38,9 @@ endif()
 
 install(TARGETS P7
         RUNTIME DESTINATION "bin"
-        ARCHIVE DESTINATION "lib" 
+        ARCHIVE DESTINATION "lib"
         LIBRARY DESTINATION "lib")
-        
-install(DIRECTORY Headers/ 
+
+install(DIRECTORY Headers/
         DESTINATION include/P7
         FILES_MATCHING PATTERN "*.h")

ports/libp7client/CONTROL
@@ -1,4 +1,5 @@
 Source: libp7client
-Version: 5.2-1
+Version: 5.6
+Homepage: https://baical.net/
 Description: Open source, cross-platform, fastest library for sending logs, telemetry & trace data from your application.
-Supports: x86|x64
+Supports: !(arm|uwp|osx)

ports/libp7client/fix-runtime-library.patch
@@ -1,76 +1,0 @@
-diff --git a/Sources/P7ClientDll.vcxproj b/Sources/P7ClientDll.vcxproj
-index 9fab3d0..a7035d9 100644
---- a/Sources/P7ClientDll.vcxproj
-+++ b/Sources/P7ClientDll.vcxproj
-@@ -112,7 +112,7 @@
-       <AdditionalIncludeDirectories>$(ProjectDir)\..\Shared\;$(ProjectDir)\..\Shared\Platforms\Windows_x86;$(ProjectDir)\..\Headers;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-       <ShowIncludes>false</ShowIncludes>
-       <EnablePREfast>false</EnablePREfast>
--      <RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>
-+      <RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>
-     </ClCompile>
-     <Link>
-       <SubSystem>Windows</SubSystem>
-@@ -137,7 +137,7 @@
-       <Optimization>Disabled</Optimization>
-       <PreprocessorDefinitions>_WIN64;_DEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
-       <AdditionalIncludeDirectories>$(ProjectDir)\..\Shared\;$(ProjectDir)\..\Shared\Platforms\Windows_x86;$(ProjectDir)\..\Headers;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
--      <RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>
-+      <RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>
-     </ClCompile>
-     <Link>
-       <SubSystem>Windows</SubSystem>
-@@ -165,7 +165,7 @@
-       <PreprocessorDefinitions>WIN32;NDEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
-       <AdditionalIncludeDirectories>$(ProjectDir)\..\Shared\;$(ProjectDir)\..\Shared\Platforms\Windows_x86;$(ProjectDir)\..\Headers;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-       <DebugInformationFormat>ProgramDatabase</DebugInformationFormat>
--      <RuntimeLibrary>MultiThreaded</RuntimeLibrary>
-+      <RuntimeLibrary>MultiThreadedDll</RuntimeLibrary>
-     </ClCompile>
-     <Link>
-       <SubSystem>Windows</SubSystem>
-@@ -195,7 +195,7 @@
-       <PreprocessorDefinitions>_WIN64;NDEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
-       <AdditionalIncludeDirectories>$(ProjectDir)\..\Shared\;$(ProjectDir)\..\Shared\Platforms\Windows_x86;$(ProjectDir)\..\Headers;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-       <DebugInformationFormat>ProgramDatabase</DebugInformationFormat>
--      <RuntimeLibrary>MultiThreaded</RuntimeLibrary>
-+      <RuntimeLibrary>MultiThreadedDll</RuntimeLibrary>
-     </ClCompile>
-     <Link>
-       <SubSystem>Windows</SubSystem>
-diff --git a/Sources/P7ClientLib.vcxproj b/Sources/P7ClientLib.vcxproj
-index 3b46bab..725f8a4 100644
---- a/Sources/P7ClientLib.vcxproj
-+++ b/Sources/P7ClientLib.vcxproj
-@@ -161,6 +161,7 @@
-       <AdditionalIncludeDirectories>$(ProjectDir)\..\Shared\;$(ProjectDir)\..\Shared\Platforms\Windows_x86;$(ProjectDir)\..\Headers;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-       <ShowIncludes>false</ShowIncludes>
-       <EnablePREfast>false</EnablePREfast>
-+      <RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>
-     </ClCompile>
-     <Link>
-       <SubSystem>Windows</SubSystem>
-@@ -181,6 +182,7 @@
-       <Optimization>Disabled</Optimization>
-       <PreprocessorDefinitions>_WIN64;_DEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
-       <AdditionalIncludeDirectories>$(ProjectDir)\..\Shared\;$(ProjectDir)\..\Shared\Platforms\Windows_x86;$(ProjectDir)\..\Headers;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-+      <RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>
-     </ClCompile>
-     <Link>
-       <SubSystem>Windows</SubSystem>
-@@ -204,6 +206,7 @@
-       <PreprocessorDefinitions>WIN32;NDEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
-       <AdditionalIncludeDirectories>$(ProjectDir)\..\Shared\;$(ProjectDir)\..\Shared\Platforms\Windows_x86;$(ProjectDir)\..\Headers;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-       <DebugInformationFormat>ProgramDatabase</DebugInformationFormat>
-+      <RuntimeLibrary>MultiThreaded</RuntimeLibrary>
-     </ClCompile>
-     <Link>
-       <SubSystem>Windows</SubSystem>
-@@ -255,6 +258,7 @@
-       <PreprocessorDefinitions>_WIN64;NDEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
-       <AdditionalIncludeDirectories>$(ProjectDir)\..\Shared\;$(ProjectDir)\..\Shared\Platforms\Windows_x86;$(ProjectDir)\..\Headers;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-       <DebugInformationFormat>ProgramDatabase</DebugInformationFormat>
-+      <RuntimeLibrary>MultiThreaded</RuntimeLibrary>
-     </ClCompile>
-     <Link>
-       <SubSystem>Windows</SubSystem>

ports/libp7client/portfile.cmake
@@ -1,118 +1,27 @@
-include(vcpkg_common_functions)
-
-if (NOT VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_UWP)
-    message(FATAL_ERROR "Unsupported system: ${VCPKG_CMAKE_SYSTEM_NAME}")
-endif()
-
-if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    set(BUILD_ARCH "Win32")
-    set(ARCH "32")
-elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    set(BUILD_ARCH "x64")
-    set(ARCH "64")
-elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
-    message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
-elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
-    message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
-else()
-    message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
-endif()
-
-set(VERSION 5.2)
+vcpkg_fail_port_install(ON_ARCH "arm" ON_TARGET "uwp" "osx")
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://baical.net/files/libP7Client_v${VERSION}.zip"
-    FILENAME "libP7Client_v${VERSION}.zip"
-    SHA512 9744b9c3f091db90aca3485408d3e1169317152ea353ab3845cd7cfb9d61d105b55be17ad83c5970e01d7d0f37566313bc18c0f8a4c16bcd1582cd7a5ea29b87
+    URLS http://baical.net/files/libP7Client_v5.6.zip
+    FILENAME libP7Client_v5.6.zip
+    SHA512 992256854b717a45ae9e11ed16aa27b8b054de97718f027664634597d756aa26fe10dcad765cde7695802c90def46461abbcbfde81923fdd40ea2b659e1a8240
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE} 
+    ARCHIVE "${ARCHIVE}"
     NO_REMOVE_ONE_LEVEL
-    REF ${VERSION}
-    PATCHES
-        "fix-runtime-library.patch"
-)
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    vcpkg_build_msbuild(
-        PROJECT_PATH ${SOURCE_PATH}/Sources/P7ClientLib.vcxproj
-        PLATFORM ${BUILD_ARCH}
-        RELEASE_CONFIGURATION 
-        DEBUG_CONFIGURATION 
-        OPTIONS
-            "/p:NoWarn=C4996" 
-    )
-
-    file(GLOB LIB_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Binaries/*${ARCH}.lib")
-    file(GLOB D_LIB_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Binaries/*${ARCH}d.lib")
-else()
-    vcpkg_build_msbuild(
-        PROJECT_PATH ${SOURCE_PATH}/Sources/P7ClientDll.vcxproj
-        PLATFORM ${BUILD_ARCH}
-        #RELEASE_CONFIGURATION 
-        #DEBUG_CONFIGURATION 
-        OPTIONS
-            "/p:NoWarn=C4996"
-    )
-
-    file(GLOB DLL_LIB_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Binaries/*${ARCH}.dll.lib")
-    file(GLOB D_DLL_LIB_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Binaries/*${ARCH}d.dll.lib")
-
-    file(GLOB DLL_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Binaries/*${ARCH}.dll")
-    file(GLOB D_DLL_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Binaries/*${ARCH}d.dll")
-
-    file(GLOB PDB_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Binaries/*${ARCH}.pdb")
-    file(GLOB D_PDB_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Binaries/*${ARCH}d.pdb")
-
-endif()
-
-file(GLOB HEADER_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Headers/*.h")
-file(INSTALL
-    ${HEADER_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include/P7
 )
 
-#file(GLOB EXE_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Binaries/*${ARCH}.exe")
-#file(GLOB D_EXE_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/Binaries/*${ARCH}d.exe")
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
-file(INSTALL
-    ${LIB_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-)
-file(INSTALL
-    ${D_LIB_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-)
-file(INSTALL
-    ${DLL_LIB_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-)
-file(INSTALL
-    ${D_DLL_LIB_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-)
-file(INSTALL
-    ${DLL_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin
-)
-file(INSTALL
-    ${D_DLL_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
-)
-file(INSTALL
-    ${PDB_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin
-)
-file(INSTALL
-    ${D_PDB_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
 )
 
-file(INSTALL ${SOURCE_PATH}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/libp7client RENAME copyright)
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-endif()
-
+vcpkg_install_cmake()
 vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+file(INSTALL ${SOURCE_PATH}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)

ports/nvtt/CONTROL
@@ -1,5 +1,7 @@
 Source: nvtt
 Version: 2.1.2
+Port-Version: 1
 Homepage: https://github.com/castano/nvidia-texture-tools
 Description: Texture processing tools with support for Direct3D 10 and 11 formats.
 Supports: !x86
+Build-Depends: libsquish

ports/nvtt/portfile.cmake
@@ -15,6 +15,7 @@ vcpkg_from_github(
         squish.patch
         fix-build-error.patch
         add-compile-options-for-osx.patch
+        skip-building-libsquish.patch
 )
 
 vcpkg_configure_cmake(

ports/nvtt/skip-building-libsquish.patch
@@ -1,0 +1,13 @@
+diff --git a/extern/CMakeLists.txt b/extern/CMakeLists.txt
+index 911c19e..6f13de4 100644
+--- a/extern/CMakeLists.txt
++++ b/extern/CMakeLists.txt
+@@ -5,7 +5,6 @@ ADD_SUBDIRECTORY(EtcLib)
+ ADD_SUBDIRECTORY(rg_etc1_v104)
+ #ADD_SUBDIRECTORY(etcpack)
+ 
+-ADD_SUBDIRECTORY(libsquish-1.15)
++#ADD_SUBDIRECTORY(libsquish-1.15)
+ 
+ ADD_SUBDIRECTORY(CMP_Core)
+-
hafenkran/vcpkg

commit f8801dd4a4e33360bb05f45b9f0e4bdf5c2da841

[vcpkg baseline][osgearth/gdal] Re-fix dependency gdal (#16904)
* [osgearth] Re-fix dependency gdal

* [gdal] Add dependency freexl on UNIX

* update version record

* fix patch

* convert to manifest

* update version record

* re-fix

* update version record

* add dependency freexl

* update version record

* Disable freexl

Co-authored-by: Robert Schumacher <roschuma@microsoft.com>

* Don't require freexl on Windows

Co-authored-by: Robert Schumacher <roschuma@microsoft.com>

* Revert osgearth change

* Fix the baseline version

* Fix osgearth baseline version

* [protouf] Automatic set protobuf_MODULE_COMPATIBLE to include the module file

* Set protobuf_MODULE_COMPATIBLE in wrapper

* Re-fix protobuf

* update version record

Co-authored-by: Victor Romero <romerosanchezv@gmail.com>
Co-authored-by: Robert Schumacher <roschuma@microsoft.com>
Co-authored-by: PhoebeHui <20694052+PhoebeHui@users.noreply.github.com>

ports/protobuf/portfile.cmake
@@ -83,6 +83,11 @@ else()
     file(COPY ${CURRENT_HOST_INSTALLED_DIR}/tools/${PORT} DESTINATION ${CURRENT_PACKAGES_DIR}/tools)
 endif()
 
+vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/share/${PORT}/protobuf-config.cmake
+    "if(protobuf_MODULE_COMPATIBLE)"
+    "if(ON)"
+)
+
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     protobuf_try_remove_recurse_wait(${CURRENT_PACKAGES_DIR}/bin)
     protobuf_try_remove_recurse_wait(${CURRENT_PACKAGES_DIR}/debug/bin)

ports/gdal/CONTROL
@@ -1,15 +1,0 @@
-Source: gdal
-Version: 3.1.3
-Port-Version: 2
-Homepage: https://gdal.org/
-Description: The Geographic Data Abstraction Library for reading and writing geospatial raster and vector data.
-Build-Depends: proj4, libpng, geos, sqlite3, curl, expat, libpq, openjpeg, libwebp, libxml2, liblzma, netcdf-c, hdf5, zlib, libgeotiff, cfitsio, json-c (!windows)
-Supports: !arm
-
-Feature: mysql-libmariadb
-Build-Depends: libmariadb
-Description: Add mysql support using libmariadb
-
-Feature: libspatialite
-Build-Depends: libspatialite
-Description: Create or update SpatiaLite databases using libspatialite

ports/gdal/portfile.cmake
@@ -185,7 +185,7 @@ else()
     endif()
     
     set(CONF_OPTS --enable-shared=${BUILD_DYNAMIC} --enable-static=${BUILD_STATIC})
-    list(APPEND CONF_OPTS --with-proj=${CURRENT_INSTALLED_DIR} --with-libjson-c=${CURRENT_INSTALLED_DIR})
+    list(APPEND CONF_OPTS --with-proj=${CURRENT_INSTALLED_DIR} --with-libjson-c=${CURRENT_INSTALLED_DIR} --without-freexl)
     list(APPEND CONF_OPTS --without-jasper)
     
     vcpkg_configure_make(

commit 0d4132b780ad85e875cba9a9ccd4be3a23bb1619

[vcpkg] Update vcpkg tool version 2021-05-05 (#17689)
* [vcpkg] Update vcpkg tool version 2021-05-05

* [vcpkg] Hotfix bug in Microsoft/vcpkg-tool#34

* [vcpkg-ci] Ignore results from non-target triplets

* [protobuf] Fix regression introduced in #16904

* [vcpkg] Fix bug where 'staticcrt' was ignored

Co-authored-by: Robert Schumacher <ras0219@outlook.com>

ports/liblsl/CONTROL
@@ -1,6 +1,7 @@
 Source: liblsl
 Version: 1.14.0
+Port-Version: 1
 Homepage: https://github.com/sccn/liblsl
 Description: C++ lsl library for multi-modal time-synched data transmission over the local network
-Supports: !(static & staticcrt)
+Supports: !staticcrt
 Build-Depends: pugixml

ports/protobuf/portfile.cmake
@@ -88,6 +88,12 @@ vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/share/${PORT}/protobuf-config.cmake
     "if(protobuf_MODULE_COMPATIBLE)"
     "if(ON)"
 )
+if(NOT protobuf_BUILD_LIBPROTOC)
+    vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/share/${PORT}/protobuf-module.cmake
+        "_protobuf_find_libraries(Protobuf_PROTOC protoc)"
+        ""
+    )
+endif()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     protobuf_try_remove_recurse_wait(${CURRENT_PACKAGES_DIR}/bin)

scripts/azure-pipelines/analyze-test-results.ps1
@@ -100,6 +100,10 @@ function build_test_results {
 
     $allTestResults = @{ }
     foreach ( $test in $xmlCollection.test) {
+        if (!$test.name.endswith(":$triplet"))
+        {
+            continue
+        }
         $name = ($test.name -replace ":.*$")
 
         # Reconstruct the original BuildResult enumeration (defined in Build.h)

scripts/bootstrap.ps1
@@ -45,7 +45,7 @@ while (!($vcpkgRootDir -eq "") -and !(Test-Path "$vcpkgRootDir\.vcpkg-root"))
 
 Write-Verbose "Examining $vcpkgRootDir for .vcpkg-root - Found"
 
-& "$scriptsDir/tls12-download.exe" github.com "/microsoft/vcpkg-tool/releases/download/2021-02-24-d67989bce1043b98092ac45996a8230a059a2d7e/vcpkg.exe" "$vcpkgRootDir\vcpkg.exe"
+& "$scriptsDir/tls12-download.exe" github.com "/microsoft/vcpkg-tool/releases/download/2021-05-05-9f849c4c43e50d1b16186ae76681c27b0c1be9d9/vcpkg.exe" "$vcpkgRootDir\vcpkg.exe"
 Write-Host ""
 
 if ($LASTEXITCODE -ne 0)

scripts/bootstrap.sh
@@ -278,8 +278,8 @@ else
 fi
 
 # Do the build
-vcpkgToolReleaseTag="2021-02-24-d67989bce1043b98092ac45996a8230a059a2d7e"
-vcpkgToolReleaseSha="0b0f2cd40aa92556c28e1dfa9f8356313a859fb6acb6c730821a06bd9d554ab1ea0622da4026ea66fb11b9ed4fda1316cc388067d51ff1f09af85d033fe4323d"
+vcpkgToolReleaseTag="2021-05-05-9f849c4c43e50d1b16186ae76681c27b0c1be9d9"
+vcpkgToolReleaseSha="2b85eb0da65221d207a5023eda0d4da74258d7fb5db9e211718efb2573673daa3fa98a75af4a570595f12467a8f7e7759a3be01b33598a4fb6d4203bf83949ef"
 vcpkgToolReleaseTarball="$vcpkgToolReleaseTag.tar.gz"
 vcpkgToolUrl="https://github.com/microsoft/vcpkg-tool/archive/$vcpkgToolReleaseTarball"
 baseBuildDir="$vcpkgRootDir/buildtrees/_vcpkg"

scripts/vcpkg_get_tags.cmake
@@ -12,6 +12,7 @@ VCPKG_VISUAL_STUDIO_PATH=${VCPKG_VISUAL_STUDIO_PATH}
 VCPKG_CHAINLOAD_TOOLCHAIN_FILE=${VCPKG_CHAINLOAD_TOOLCHAIN_FILE}
 VCPKG_BUILD_TYPE=${VCPKG_BUILD_TYPE}
 VCPKG_LIBRARY_LINKAGE=${VCPKG_LIBRARY_LINKAGE}
+VCPKG_CRT_LINKAGE=${VCPKG_CRT_LINKAGE}
 e1e74b5c-18cb-4474-a6bd-5c1c8bc81f3f")
 
     # Just to enforce the user didn't set it in the triplet file

scripts/azure-pipelines/test-modified-ports.ps1
@@ -142,7 +142,16 @@ if ($Triplet -in @('x64-uwp', 'arm64-windows', 'arm-uwp', 'x64-windows', 'x64-wi
     .\vcpkg.exe install yasm-tool:x86-windows @commonArgs
 }
 
-& "./vcpkg$executableExtension" ci $Triplet --x-xunit=$xmlFile --exclude=$skipList --failure-logs=$failureLogs @commonArgs
+if ($Triplet -in @('x64-windows', 'x64-osx', 'x64-linux'))
+{
+    # WORKAROUND: These triplets are native-targetting which triggers an issue in how vcpkg handles the skip list.
+    # The workaround is to pass the skip list as host-excludes as well.
+    & "./vcpkg$executableExtension" ci $Triplet --x-xunit=$xmlFile --exclude=$skipList --host-exclude=$skipList --failure-logs=$failureLogs @commonArgs
+}
+else
+{
+    & "./vcpkg$executableExtension" ci $Triplet --x-xunit=$xmlFile --exclude=$skipList --failure-logs=$failureLogs @commonArgs
+}
 & "$PSScriptRoot/analyze-test-results.ps1" -logDir $xmlResults `
     -triplet $Triplet `
     -baselineFile .\scripts\ci.baseline.txt